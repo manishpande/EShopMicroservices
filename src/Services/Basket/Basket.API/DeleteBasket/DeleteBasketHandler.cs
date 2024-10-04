1a60a00a1a69dd30e94e49c174b83b518370929e
Manish Pande <manishpande10@gmail.com>
2024-10-04 11:29:35 +0530

    Order Microservice updated

---
+++ src/Services/Basket/Basket.API/DeleteBasket/DeleteBasketHandler.cs
@@ -0,0 +1,19 @@
+﻿
+using Basket.API.Data;
+
+namespace Basket.API.DeleteBasket
+{
+    public record DeleteBasketCommand(string UserName) : ICommand<DeleteBasketResult>;
+    public record DeleteBasketResult(bool IsSuccess);
+    public class DeleteBasketCommandHandler(IBasketRepository repository)
+    : ICommandHandler<DeleteBasketCommand, DeleteBasketResult>
+    {
+        public async Task<DeleteBasketResult> Handle(DeleteBasketCommand command, CancellationToken cancellationToken)
+        {
+            // TODO: delete basket from database and cache       
+            await repository.DeleteBasket(command.UserName, cancellationToken);
+
+            return new DeleteBasketResult(true);
+        }
+    }
+}
